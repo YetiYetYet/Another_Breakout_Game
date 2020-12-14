f789d228c864c572a80021c0c4397bcc4f73f1b4
YetiYetYet <alexisbehier22@gmail.com>
2020-12-14 23:20:28 +0100

    Adding paddle movement and ball velocity

---
+++ Assets/Paddle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    private Rigidbody _rb;
+    [SerializeField]
+    private Vector3 targetPos;
+    private Camera _camera;
+    private Plane _plane;
+    public float speed = 1;
+    public float limit = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = transform.position;
+        _camera = Camera.main;
+        _plane = new Plane(Vector3.forward, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+        if (_plane.Raycast(ray, out float distance))
+        {
+            targetPos.x = Mathf.Clamp(ray.GetPoint(distance).x, -limit, limit);
+            
+        }
+        transform.position = Vector3.Lerp(transform.position, targetPos, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.velocity = new Vector3(10f, 10f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
